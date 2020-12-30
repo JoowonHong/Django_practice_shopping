8fa41516bc37e0a9c4535358230251c9bb6ac073
Joowon Hong <ghdwn6687@gmail.com>
2020-12-30 12:19:55 +0900

    Data migrate

--- fc_django/fcuser/models.py
+++ fc_django/fcuser/models.py
@@ -3,4 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Fcuser(models.Model):
-    email = models.EmailField()
+    email = models.EmailField(verbose_name='e-mail')
+    password = models.CharField(max_length=64,verbose_name='Password')
+    register_date= models.DateTimeField(auto_now_add=True,verbose_name='등록날')
+
+    class Meta:
+        db_table = 'fastcampus_fcuser'
+        verbose_name='사용자'
+        verbose_name_plural='사용자'
